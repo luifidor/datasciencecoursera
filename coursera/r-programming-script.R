322e11c18ff9fb23370247f586e84c717ebaea4e
noel.figuera <noel.figuera@wbadev.com>
2017-08-08 16:03:20 +0200

    Add new lines to the R file

--- coursera/r-programming-script.R
+++ coursera/r-programming-script.R
@@ -40,6 +40,110 @@ x<-1:3
 names(x) <- c("foo", "bar", "do")
 x
 
+## removing na values
+x<- c(1, 2, NA, 4, NA, 5)
+bad <- is.na(x)
+x[!bad]
+
+## remove items that are not matching across vectors
+x<-c(1, 2, NA, 4, NA, 5)
+y<-c("a","b",NA,"d", NA, "f")
+length(x)
+length(y)
+good <- complete.cases(x,y)
+good
+x[good]
+y[good]
+
+## list
+x <- list(2, "a", "b", TRUE)
+
+
+# test
+
+x <- read.csv("hw1_data.csv")
+sum(is.na(x$Ozone))
+rle(is.na(x$Ozone))
+
+x[!is.na(x$Ozone),]$Ozone
+
+mean(x[!is.na(x$Ozone),]$Ozone)
+
+x[!is.na(x$Ozone) & !is.na(x$Temp) & x$Ozone > 31 & x$Temp > 90,]$Solar.R
+
+mean(x[!is.na(x$Ozone) & !is.na(x$Temp) & x$Ozone > 31 & x$Temp > 90,]$Solar.R)
+
+max(x[!is.na(x$Ozone) & x$Month ==5,]$Ozone)
+
+
+## Control 
+
+x <- 5
+
+if(x > 3) {
+        y <- 10
+        
+} else {
+        y <- 0
+}
+
+y
+
+## for loops vectors
+
+x<-c("a", "b", "c", "d")
+
+for (i in seq_along(x)) {
+        print(x[i])
+}
+
+## for loops matrix
+
+x<-matrix(1:6, 2, 3)
+
+for (i in seq_len(nrow(x))) {
+        for (j in seq_len(ncol(x))) {
+                print(x[i,j])
+        }
+}
+
+# while loop
+
+count <- 0
+
+while (count < 10) {
+        print(count)
+        count <- count + 1
+}
+
+
+
+add2 <- function(x,y) {
+        x + y
+}
+
+above <- function(x, y) {
+        
+        use <- x > y
+        
+        x[use]
+        
+}
+
+columnmean <- function (y, removeNA = TRUE) {
+        nc <- ncol(y)
+        means <- numeric(nc)
+        for (i in 1:nc) {
+                means[i] <- mean(y[, i], na.rm = removeNA)
+        }
+        means
+        
+}
+
+
+
+
+
 # connect R to SQL Server 
 ## https://www.red-gate.com/simple-talk/sql/reporting-services/making-data-analytics-simpler-sql-server-and-r/
 ## https://support.rstudio.com/hc/en-us/articles/214510788-Setting-up-R-to-connect-to-SQL-Server-
@@ -48,10 +152,24 @@ library(RODBC)
 ??RODBC
 cn <-odbcDriverConnect("driver=SQL Server;server=swbbesqld01.wbad.group;database=minibid; uid=minibid_submit; pwd=Wbad2016")
 bidlog <- sqlFetch(cn, 'fact.bidlog', colnames=FALSE,
-                             rows_at_time=1000)
+                   rows_at_time=1000)
 hist(as.numeric(bidlog$count_processing_days), xlim=c(0,30),breaks = 7)
 
 ## Analyze the distribution of processing days across years
 hist(as.numeric(bidlog[(bidlog$count_processing_days < 100 & bidlog$close_date_cy == 2015), ]$count_processing_days), ylim=c(0, 0.05), probability = TRUE)
 hist(as.numeric(bidlog[(bidlog$count_processing_days < 100 & bidlog$close_date_cy == 2016), ]$count_processing_days), ylim=c(0, 0.05), probability = TRUE)
 hist(as.numeric(bidlog[(bidlog$count_processing_days < 100 & bidlog$close_date_cy == 2017), ]$count_processing_days),  ylim=c(0, 0.05), probability = TRUE)
+
+sapply(bidlog$award_localdn_var, max, na.rm=TRUE)
+
+summary(bidlog$award_localdn_var)
+
+hist(as.numeric(bidlog[(bidlog$award_localdn_var < 4e+06 & bidlog$award_localdn_var > 150000), ]$award_localdn_var), xlim = c(150000, 4e06),probability = TRUE)
+
+which.max(bidlog$award_localdn_var)
+bidlog[which.max(bidlog$award_localdn_var),]
+
+
+install.packages('pastecs')
+library (Hmisc)
+describe(bidlog$award_localdn_var)
