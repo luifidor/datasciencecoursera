9271bd6f7458fec3feb9da5a34c1d8e812f3d15b
noel.figuera <noel.figuera@wbadev.com>
2017-08-29 16:39:29 +0200

    Class number 4

--- coursera/exam3.R
+++ coursera/exam3.R
@@ -188,3 +188,100 @@ rev_v <- sapply(split(outcome[,11], outcome$State),max, na.rm = TRUE)
 
 
 head(rankall("heart attack", 20), 10)
+
+
+
+rankall <- function(outcome, num) {
+        ## Read outcome data
+        
+        ## Check that state and outcome are valid
+        
+        ## Return hospital name is tha tstate with lowest 30 day death
+        ## rate
+        
+        # "heart attack" , "heart failure" and "pneumonia"
+        outcome_id = NULL
+        
+        # check if outcome measure is valid
+        if (outcome == "heart attack") {
+                outcome_id = 11
+        } else if (outcome == "heart failure") {
+                outcome_id = 17
+        } else if ( outcome == "pneumonia") {
+                outcome_id = 23
+        } else {
+                stop ("Not a valid outcome measure")
+        }
+        
+        # load relevant data
+        data <- read.csv("\\\\wbad.group\\dfs_WB\\10_Home\\UWBBENOELF\\DATA\\Desktop\\GIT Folder\\coursera\\coursera\\outcome-of-care-measures.csv", colClasses = "character")
+        suppressWarnings(data[,outcome_id] <- as.numeric(data[,outcome_id]))
+        
+        # check if state is valid
+        if (is.na(match(state, data[,7]))) {
+                stop ("Not a valid state")
+        }
+        
+        #filter by state and order the data ascending
+        data <- data[complete.cases(data[,outcome_id]),]
+        
+       
+        
+        f_states <- levels(factor(data$State))
+        
+        r <- NULL
+        
+        for (s in f_states) {
+                
+                t <- data[data$State == s,]
+                
+                rank_v = order(t[,outcome_id])
+                t <- cbind(t[rank_v,], rank = 1:length(rank_v), length(rank_v):1)
+                if (is.null(r)) {
+                        r <- t
+                } else { 
+                        r <- rbind(r, t) 
+                }
+                
+        }
+        
+        #r[,c(7,2,outcome_id,47,48)]
+        
+        #return results
+        if (num == "best") {
+                r <- r[r$rank == 1, ]
+        } else if (num == "worst") {
+                r <- r[r$reverse == 1, ]
+        } else if (num == "all") {
+                
+        } else {
+                r <- r[r$rank == as.numeric(num), ]
+        }
+        
+        r <- r[,c(7,2,outcome_id, 47, 48)]
+        colnames(r) <- c("State", "Hospital", "Rate", "Inv Rank")
+        r
+        #r[,c(7,2,outcome_id)]
+}
+
+best("SC", "heart attack")
+best("NY", "pneumonia")
+best("AK", "pneumonia")
+rankhospital("NC", "heart attack", "worst")
+rankhospital("WA", "heart attack", 7)
+rankhospital("TX", "pneumonia", 10)
+rankhospital("NY", "heart attack", 7)
+r <- rankall("heart attack", 4)
+as.character(subset(r, state == "HI")$hospital)
+r <- rankall("pneumonia", "all")
+r[r$State == "NJ",]
+r <- rankall("heart failure", 10)
+r[r$State == "NV",]
+as.character(subset(r, State == "NJ")$Hospital)
+
+set.seed(1)
+rpois(5, 2)
+set.seed(10)
+x <- rep(0:1, each = 5)
+e <- rnorm(10, 0, 20)
+y <- 0.5 + 2 * x + e
